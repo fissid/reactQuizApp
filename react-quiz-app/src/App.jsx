b60e863c283510ed1bd1eda68bc0c75f2cab3a15
fissid <fissid.ss@gmail.com>
2024-01-06 21:18:09 +0300

    updates

--- react-quiz-app/src/App.jsx
+++ react-quiz-app/src/App.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const INITIALSTATE = {};
+function reducer() {}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, INITIALSTATE);
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const resp = await fetch("http://localhost:8000/questions");
+        const data = await resp.json();
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchQuestions();
+  }, []);
   return (
     <div className="app">
       <Header />

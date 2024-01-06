4227da49fcb500e342172de3f864e4b2b11f3083
fissid <fissid.ss@gmail.com>
2024-01-06 21:05:47 +0300

    updates

--- react-quiz-app/src/App.jsx
+++ react-quiz-app/src/App.jsx
@@ -1,3 +1,14 @@
+import Header from "./Header";
+import Main from "./Main";
+
 export default function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
+    </div>
+  );
 }
